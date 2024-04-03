f0667d1ab500e91d2c69f03820b041ec5ed2c7a5
Caique L. Fernandes <caique.l.f9.clf@gmail.com>
2024-04-03 18:01:12 -0400

    Evitando ataques de SQL Injection

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', async(req, res)=>{
-
+app.get('/:id', async(req, res)=>{
+    const {id} = req.params
 
    try {
 
-        const resultado = await pool.query('select * from empresas')
+        const query = 'select * from empresas where id = $1 '
+        const params = [id]
+
+        const resultado = await pool.query(query, params)
 
         return res.json(resultado.rows)
+
    } catch (error) {
       return res.status(400).json(error.message)
    }
